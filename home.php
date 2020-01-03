446bfe88bc2f800bb46e70c16992e691e3457727
Janith Harinda <49230408+jharinda@users.noreply.github.com>
2020-01-03 13:10:35 +0530

    Create home.php

---
+++ home.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>SriBoard</title>
+    <link rel="shortcut icon" href="flag-round-250.png" type="image/png">
+    <link rel="stylesheet" href="style.css">
+    <script type="text/javascript" src="script.js" defer></script>
+  </head>
+  <body>
+    <div class="piano">
+      <?php
+      include(header.html);
+
+       ?>
+
+      <div data-note="C5" class="key white"></div>
+      <div data-note="Db5" class="key black"></div>
+      <div data-note="D5" class="key white"></div>
+      <div data-note="Eb5" class="key black"></div>
+      <div data-note="E5" class="key white"></div>
+      <div data-note="F5" class="key white"></div>
+      <div data-note="Gb5" class="key black"></div>
+      <div data-note="G5" class="key white"></div>
+      <div data-note="Ab5" class="key black"></div>
+      <div data-note="A5" class="key white"></div>
+      <div data-note="Bb5" class="key black"></div>
+      <div data-note="B5" class="key white"></div>
+      <div data-note="C6" class="key white"></div>
+      <div data-note="Db6" class="key black"></div>
+      <div data-note="D6" class="key white"></div>
+      <div data-note="Eb6" class="key black"></div>
+      <div data-note="E6" class="key white"></div>
+      <div data-note="F6" class="key white"></div>
+      <div data-note="Gb6" class="key black"></div>
+      <div data-note="G6" class="key white"></div>
+      <div data-note="Ab6" class="key black"></div>
+      <div data-note="A6" class="key white"></div>
+      <div data-note="Bb6" class="key black"></div>
+      <div data-note="B6" class="key white"></div>
+      <div data-note="C7" class="key white"></div>
+    </div>
+    <audio id="C5" src="notes/C5.mp3"></audio>
+    <audio id="Db5" src="notes/Db5.mp3"></audio>
+    <audio id="D5" src="notes/D5.mp3"></audio>
+    <audio id="Eb5" src="notes/Eb5.mp3"></audio>
+    <audio id="E5" src="notes/E5.mp3"></audio>
+    <audio id="F5" src="notes/F5.mp3"></audio>
+    <audio id="Gb5" src="notes/Gb5.mp3"></audio>
+    <audio id="G5" src="notes/G5.mp3"></audio>
+    <audio id="Ab5" src="notes/Ab5.mp3"></audio>
+    <audio id="A5" src="notes/A5.mp3"></audio>
+    <audio id="Bb5" src="notes/Bb5.mp3"></audio>
+    <audio id="B5" src="notes/B5.mp3"></audio>
+    <audio id="C6" src="notes/C6.mp3"></audio>
+    <audio id="Db6" src="notes/Db6.mp3"></audio>
+    <audio id="D6" src="notes/D6.mp3"></audio>
+    <audio id="Eb6" src="notes/Eb6.mp3"></audio>
+    <audio id="E6" src="notes/E6.mp3"></audio>
+    <audio id="F6" src="notes/F6.mp3"></audio>
+    <audio id="Gb6" src="notes/Gb6.mp3"></audio>
+    <audio id="G6" src="notes/G6.mp3"></audio>
+    <audio id="Ab6" src="notes/Ab6.mp3"></audio>
+    <audio id="A6" src="notes/A6.mp3"></audio>
+    <audio id="Bb6" src="notes/Bb6.mp3"></audio>
+    <audio id="B6" src="notes/B6.mp3"></audio>
+    <audio id="C7" src="notes/C7.mp3"></audio>
+  </body>
+</html>
